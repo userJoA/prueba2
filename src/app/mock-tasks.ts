9d6a0cb84dcc04242fa0ad8f66d0c35a144f933c
userJoA <jo1gallardo@gmail.com>
2022-01-12 15:43:22 -0300

    creacion del boton

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+
+import {Task} from "./Task"
+export const TASK:Task[] =[
+{
+    id: 1,
+    text: "terminar primer modulo de angular",
+    day: 'Agosto 5 a las 12:00',
+    reminder:true
+}, 
+{
+    id: 2,
+    text: "Hacer compras para la cena",
+    day: 'Agosto 5 a las 17:00',
+    reminder:false
+},
+{
+    id: 3,
+    text: "terminar mi proyecto",
+    day:  "Agosto 5 a las 19:00",
+    reminder:false
+},
+{
+    id: 4,
+    text : "Cenar",
+    day: "Agosto 5 a las 21:00",
+    reminder: false
+}  
+]
